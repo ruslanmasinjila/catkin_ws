2760374b4ac1cec6574088191309b7063b54954c
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2025-02-03 13:30:24 -0330

    Added Base link to scratch

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ruslan/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
-export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
 export PWD='/home/ruslan/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/ruslan/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/ruslan/catkin_ws/src:$ROS_PACKAGE_PATH"
